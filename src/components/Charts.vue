c224840be47442d8da3f2164d79e7c068226789f
Eyasu Hailegebriel <IyasuH>
2025-05-31 10:44:09 +0300

    Chart data fetching from API for given_name and father_name

--- src/components/Charts.vue
+++ src/components/Charts.vue
@@ -1,59 +1,50 @@
 <script setup>
-import { ref } from 'vue';
+import { onMounted, ref } from 'vue';
 import SectionTitle from './Subcomponents/SectionTitle.vue';
 import SectionSubtitle from './Subcomponents/SectionSubtitle.vue';
 import ChartButton from './Subcomponents/ChartButton.vue';
 import ChartItem from './Subcomponents/ChartItem.vue';
+import { getTopNames, getTopFNames } from '../services/api';
+
+defineProps({
+    total_population: Number
+})
 
 const activeChart = ref("Given Names")
 
 const name_types = ["Given Names", "Father's Names", "Grandfather's Names"]
-const given_names_chart = [
-    {name: "Abebe", amount: 3200, percent: 80},
-    {name: "Mohammed", amount: 2800, percent: 70},
-    {name: "Tigist", amount: 2500, percent: 62},
-    {name: "Kidus", amount: 2200, percent: 55},
-    {name: "Hiwot", amount: 2000, percent: 50},
-    {name: "Dawit", amount: 1800, percent: 45},
-    {name: "Rahel", amount: 1600, percent: 40},
-    {name: "Yonas", amount: 1500, percent: 37},
-    {name: "Meron", amount: 1400, percent: 35},
-    {name: "Henok", amount: 1300, percent: 32}
-]
-const father_names_chart = [
-    {name: "Kebede", amount: 3200, percent: 90},
-    {name: "Mohammed", amount: 2800, percent: 70},
-    {name: "Tigist", amount: 2500, percent: 62},
-    {name: "Kidus", amount: 2200, percent: 55},
-    {name: "Hiwot", amount: 2000, percent: 50},
-    {name: "Dawit", amount: 1800, percent: 45},
-    {name: "Rahel", amount: 1600, percent: 40},
-    {name: "Yonas", amount: 1500, percent: 37},
-    {name: "Meron", amount: 1400, percent: 35},
-    {name: "Henok", amount: 1300, percent: 32}
-]
-const grand_names_chart = [
-    {name: "Getachew", amount: 3200, percent: 100},
-    {name: "Mohammed", amount: 2800, percent: 70},
-    {name: "Tigist", amount: 2500, percent: 62},
-    {name: "Kidus", amount: 2200, percent: 55},
-    {name: "Hiwot", amount: 2000, percent: 50},
-    {name: "Dawit", amount: 1800, percent: 45},
-    {name: "Rahel", amount: 1600, percent: 40},
-    {name: "Yonas", amount: 1500, percent: 37},
-    {name: "Meron", amount: 1400, percent: 35},
-    {name: "Henok", amount: 1300, percent: 32}
-]
+const chart_items = ref([])
+const given_names_chart = ref([])
+const father_names_chart = ref([])
+const grand_names_chart = ref([
+    {name: "Getachew", count: 3200, percent: 100},
+    {name: "Mohammed", count: 2800, percent: 70},
+    {name: "Tigist", count: 2500, percent: 62},
+    {name: "Kidus", count: 2200, percent: 55},
+    {name: "Hiwot", count: 2000, percent: 50},
+    {name: "Dawit", count: 1800, percent: 45},
+    {name: "Rahel", count: 1600, percent: 40},
+    {name: "Yonas", count: 1500, percent: 37},
+    {name: "Meron", count: 1400, percent: 35},
+    {name: "Henok", count: 1300, percent: 32}
+])
 
-const chart_items = ref(given_names_chart)
+onMounted(async () => {
+    given_names_chart.value = await getTopNames()
+    father_names_chart.value = await getTopFNames()
+    chart_items.value = given_names_chart.value // default to Given Names
+    console.log(`father_names_chart.value: ${given_names_chart.value[0].count}`)
+})
+
+
 
 const changeActiveChart = (chartName) => {
     console.log(`Changed active chart to ${chartName}`)
     activeChart.value = chartName;
     if (chartName === "Given Names"){
-        chart_items.value = given_names_chart
+        chart_items.value = given_names_chart.value
     } else if (chartName === "Father's Names") {
-        chart_items.value = father_names_chart
+        chart_items.value = father_names_chart.value
     } else if (chartName === "Grandfather's Names") {
         chart_items.value = grand_names_chart
     }
@@ -68,7 +59,6 @@ const changeActiveChart = (chartName) => {
                 subtitle="Name Frequency Analysis"
                 description="Explore the most common names across different name types"
             />
-        
             <div class="border-b border-gray-200">
                 <div class="flex">
                     <div v-for="(name_, index) in name_types" :key="index">
@@ -103,8 +93,8 @@ const changeActiveChart = (chartName) => {
                         <div v-for="(item, index) in chart_items" :key="index">
                             <ChartItem 
                                 :name="item.name"
-                                :amount="item.amount"
-                                :percent="item.percent"
+                                :count="item.count.toLocaleString()"
+                                :percent="(item.count/total_population) * 7300"
                             />
                         </div>
                     </div>
